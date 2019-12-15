c30f0f4d3e329afe544fed6ff152c65be0e0388a
Glitch (spangled-earth) <none>
2019-12-15 02:07:40 +0000

    adding in gyp for backend

--- src/index.cc
+++ src/index.cc
@@ -1,6 +1,6 @@
 #include "napi.h"
 
-Napi::String Method(const Napi::CallbackInfo& info) {
+/*Napi::String Method(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   return Napi::String::New(env, "world");
 }
@@ -9,7 +9,7 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
   exports.Set(Napi::String::New(env, "hello"),
               Napi::Function::New(env, Method));
   return exports;
-}
+}*/
 
 
 
