2030711a88fbdd7a3c33b7c043248b505865ce1e
Glitch (spangled-earth) <none>
2019-12-16 14:54:21 +0000

    ✂️👆 Checkpoint ./index.js:6818284/2526

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ var { cosmiconfig } = require("cosmiconfig");
 var explorer = cosmiconfig("bcknd");
 
 //*******************************************
+
 //explorer config
 var x;
 
-function SetHttp() {}
+function SetHttp(serve, port, options = { async: false, forceHttps: true }) {
+  var HTTP = require("http");
+  if (options.forceHttps === true) {
+    
+  }
+  if (options.async === true) {
+    delete options.async;
+    AsyncSetHttp(serve,port,options);
+  }
+}
+
+//***********************************************
+
+async function AsyncSetHttp(serve, port, options) {
+  
+}
+
+//**************************************************
 
 function SetConfig() {
   explorer
